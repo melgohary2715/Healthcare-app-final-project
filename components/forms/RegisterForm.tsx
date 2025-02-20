848a875a905f4417cf8d059fb89fde56a12e5d71
Mohamed El-Gohary <melgohary2715@gmail.com>
2025-02-20 15:18:36 -0500

    g1g1

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -130,6 +130,11 @@ const RegisterForm = ({ user }: { user: User }) => {
             )}
           />
         </div>
+        {/* <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical Information</h2>
+          </div>
+        </section> */}
         <div className="flex flex-col gap-6 xl:flex-row"></div>
         <div className="flex flex-col gap-6 xl:flex-row"></div>
         <div className="flex flex-col gap-6 xl:flex-row"></div>
